1576d67465e2cac59546ca3fbe1dc288668eb51f
steve fro <sfro@uwalumni.com>
2020-07-22 16:23:32 -0500

    initial code for exercise 2 elastic search

--- workspace/exercise_2/run_exercise_2_in_ec2.sh
+++ workspace/exercise_2/run_exercise_2_in_ec2.sh
@@ -4,7 +4,7 @@
 # ie: $ export RAMPUP_HOST=3.80.86.160
 
 SERVER=$RAMPUP_HOST
-ROLE_DIR=simple_role
+ROLE_DIR=elastic_search_role
 
 if [ -z $SERVER ]; then
     echo '\n\nCould not find host. Make sure env var is set. ie: $ export RAMPUP_HOST=some.host.ip\n'
@@ -21,6 +21,6 @@ echo ''
 
 cd ../../ec2_setup/
 # echo 'running: ansible-playbook -i hosts amazon_os_playbook.yml --tags molecule-test -e ansible_host='$SERVER' -e molecule_cmd=converge -e role_under_test='$ROLE_DIR
-ansible-playbook -i hosts amazon_os_playbook.yml --tags molecule-test -e ansible_host=$SERVER -e molecule_cmd=converge -e role_under_test=role_under_test=../../$ROLE_DIR
+#ansible-playbook -i hosts amazon_os_playbook.yml --tags molecule-test -e ansible_host=$SERVER -e molecule_cmd=converge -e role_under_test=role_under_test=../../$ROLE_DIR
 
 
