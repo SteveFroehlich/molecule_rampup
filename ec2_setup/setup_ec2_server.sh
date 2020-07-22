860e3760db85b960ae9cfd0e2c9a7dab4a01f396
steve fro <sfro@uwalumni.com>
2020-07-22 15:12:01 -0500

    support to run exercise 1 in ec2

--- ec2_setup/setup_ec2_server.sh
+++ ec2_setup/setup_ec2_server.sh
@@ -3,23 +3,19 @@
 # run the playbook to setup an EC2 instance to be 
 # an automation CI server
 
-HOST_IP=$1
-if [ -z ${1+x} ]; then
-    echo "\n\nUsage: $ ./setip_ci_server.sh <playbook> <host-ip> [optional]<tags>\n\n"
+HOST_IP=$RAMPUP_HOST
+PLAYBOOK=amazon_os_playbook.yml
+
+if [ -z $HOST_IP ]; then
+    echo '\n\nmake sure env var is set: $ export RAMPUP_HOST=some.host.ip'
+    echo "Usage: $ ./setip_ci_server.sh [optional]<tags>\n\n"
     exit 1
 fi
 
-
-if [ -z ${2+x} ]; then
-    PLAYBOOK=amazon_os_playbook.yml
-else
-    PLAYBOOK=$2
-fi
-
-if [ -z ${3+x} ]; then
+if [ -z ${1+x} ]; then
     TAGS=""
 else
-    TAGS="--tags $3"
+    TAGS="--tags $1"
 fi
 
 
---
+++ workspace/exercise_1/run_exercise_1_in_ec2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# set env var with host.
+# ie: $ export RAMPUP_HOST=3.80.86.160
+
+SERVER=$RAMPUP_HOST99
+ROLE_DIR=simple_role
+
+if [ -z $SERVER ]; then
+    echo '\n\nCould not find host. Make sure env var is set. ie: $ export RAMPUP_HOST=some.host.ip\n'
+    exit 1
+fi
+
+echo ''
+echo 'submitting changes to host ' $SERVER
+
+rsync -avz $ROLE_DIR ec2-user@$SERVER:~/
+
+echo 'changes successfully sync'd to server 
+echo ''
+
+cd ../../ec2_setup/
+# echo 'running: ansible-playbook -i hosts amazon_os_playbook.yml --tags molecule-test -e ansible_host='$SERVER' -e molecule_cmd=converge -e role_under_test='$ROLE_DIR
+ansible-playbook -i hosts amazon_os_playbook.yml --tags molecule-test -e ansible_host=$SERVER -e molecule_cmd=converge -e role_under_test=role_under_test=../../$ROLE_DIR
+
+
